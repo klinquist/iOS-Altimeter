18da58071aa8860047b53a6f5e3c09a215401010
Kris Linquist <kris@linquist.net>
2014-09-15 16:30:50 -0700

    first commit

--- Altimeter/ViewController.swift
+++ Altimeter/ViewController.swift
@@ -7,12 +7,81 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    func UpdateAltitude() {
+        yourAltitudeCaptionLabel.hidden = false
+        yourAltitudeLabel.hidden = false
+        var airportBarometricPressurehPa:Float = (airportBarometricPressureTextBox.text as NSString).floatValue * 33.8638866667
+        var yourBarometricPressurehPa:Float = (yourBarometricPressureTextBox.text as NSString).floatValue * 33.8638866667
+        var yourCalculatedAlt = pow(10, log10(yourBarometricPressurehPa/airportBarometricPressurehPa)/5.2558797)-1
+        yourCalculatedAlt = yourCalculatedAlt / (-6.8755856 * pow(10,-6))
+        var yourCalculatedAltString = NSString(format: "%.0f", yourCalculatedAlt)
+        yourBarometricPressureTextBox.resignFirstResponder()
+        yourAltitudeLabel.text = yourCalculatedAltString + " ft"
+    }
 
+    @IBOutlet weak var airportBarometricPressureTextBox: UITextField!
+    
+    @IBOutlet weak var airportAltitudeTextBox: UITextField!
+    
+    @IBOutlet weak var yourBarometricPressureTextBox: UITextField!
+    
+    @IBOutlet weak var yourAltitudeLabel: UILabel!
+    
+    @IBOutlet weak var yourAltitudeCaptionLabel: UILabel!
+    
+    @IBOutlet weak var airportNameValue: UITextField!
+    
+    @IBAction func calculateButton(sender: UIButton) {
+        UpdateAltitude()
+    }
+    
+    @IBAction func retrieveButtonPressed(sender: UIButton) {
+        yourAltitudeLabel.hidden = false
+        let url = NSURL(string: "http://api.wunderground.com/api/343fae88a6936714/forecast/geolookup/conditions/q/" +  airportNameValue.text + ".json")
+        let request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+
+            var datastring = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println(datastring)
+            
+            let value = datastring
+            let pattern = "\\\"pressure_in\\\":\\\"([-+]?[0-9]*\\.?[0-9]+.)\\\""
+            for m in value =~ pattern {
+                self.airportBarometricPressureTextBox.text = m
+            }
+            let elevationpattern = "\\\"elevation\\\":\\\"([-+]?[0-9]*\\.?[0-9]+.)\\\""
+            for m in value =~ elevationpattern {
+                var AltConversion:Int = 0
+                var AltFloat = (m as NSString).floatValue
+                self.airportAltitudeTextBox.text = NSString(format: "%.0f", AltFloat)
+            }
+            
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       var altimeter = CMAltimeter()
+       var pressure:Float = 0
+       if CMAltimeter.isRelativeAltitudeAvailable() {
+            altimeter.startRelativeAltitudeUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: { data, error in
+                if !(error != nil) {
+                    pressure = data.pressure * 0.2953
+                    self.yourBarometricPressureTextBox.text = NSString(format:"%.2f", pressure)
+                    self.UpdateAltitude()
+                 
+                }
+            })
+        }
+        
+
+   
     }
 
     override func didReceiveMemoryWarning() {
