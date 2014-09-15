d396b6ea941e64f8e10c40be5f7f0e50a138bad2
Kris Linquist <kris@linquist.net>
2014-09-14 20:55:14 -0700

    Initial Commit

---
+++ Altimeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Altimeter
+//
+//  Created by Kristopher Linquist on 9/14/14.
+//  Copyright (c) 2014 Kristopher Linquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
